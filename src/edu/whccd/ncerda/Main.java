98fe0fb6f9a92d8989470300101e742abe507126
naomicerda1 <naomicerda1@gmail.com>
2020-04-30 12:32:58 -0700

    git commit: ran the debug window

---
+++ src/edu/whccd/ncerda/Main.java
@@ -0,0 +1,49 @@
+package edu.whccd.ncerda;
+import java.util.Scanner;
+
+/*
+            The purpose is to practice debugging and testing
+            author:     Naomi Cerda
+ */
+
+public class Main {
+
+    private static double d;
+
+    public static void main(String[] args) {
+
+        // 2. fix the runtime error caused by the line below by deleting, commenting, or encapsulating it in a try-catch block
+        getDouble(null,
+                null);
+
+
+        // 3. fix the logic error so it calls the getDouble() on the 100th iteration
+        for (int i = 1; i < 100; i++) {
+            if (i == 50) {
+                Scanner sc = new Scanner(System.in);
+                getDouble(sc, "Enter a double: ");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+
+    // 1. fix the syntax errors in the method below
+    public static double getDouble(Scanner sc, String prompt) {
+        d = 0.0;
+        while (true) {
+            System.out.print(prompt);
+
+            if (sc.hasNextDouble())
+            {
+                d = sc.nextDouble();
+                sc.nextLine();
+                return d;
+            }
+            else {
+                System.out.println("Error! Invalid number. Try again.");
+                sc.nextLine();
+            }
+        }
+    }
+}
